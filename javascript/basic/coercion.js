d999a4e757a3f200bbaa4f12e0dcfd2ec0f6fd76
Kumar Programming <kkyprogramming@gmail.com>
2023-12-07 21:37:06 +0000

    coercion

---
+++ javascript/basic/coercion.js
@@ -0,0 +1,19 @@
+
+// Implicit Coercion
+var num = 5;
+var str = "10";
+
+var result = num + str;
+
+console.log(result); // "510"
+console.log(typeof result); // "string"
+
+
+
+// Explicit  Coercion
+var str = "10";
+var num = parseInt(str);
+
+console.log(num); // 10
+console.log(typeof num);  // number
+
