504abd78438836ee88975da447155d06b2726bfe
Kumar Programming <kkyprogramming@gmail.com>
2023-12-23 20:58:34 +0000

    var let and const

---
+++ javascript/basic/var_let_const.js
@@ -0,0 +1,53 @@
+// VAR
+// var x  = 10;
+// function example(){
+//     if(true){
+//         var z = 30;
+//         y = 20;
+//         y = 40;
+//     }
+//     console.log(y);
+//     var y ;
+// }
+// // console.log(window.x);
+// // console.log(y);
+// // console.log(z);
+// example();
+
+
+// // LET 
+// let x  = 10;
+// function example(){
+//     let y;
+//      y = 20;
+//      y = 30;
+//      y = 40;
+//     if(true){
+//         let z = 30;
+//         console.log(z);
+//     }
+//     console.log(y);
+// }
+// // console.log(window.x);
+// // console.log(y);
+// // console.log(z);
+// example();
+
+
+// CONST 
+const x  = 10;
+function example(){
+    const y = 20;
+    y = 40; 
+    if(true){
+        const z = 30;
+        console.log(z);
+    }
+    console.log(y);
+}
+// console.log(window.x);
+// console.log(y);
+// console.log(z);
+example();
+
+
